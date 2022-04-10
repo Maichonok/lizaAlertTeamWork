f325e3810c8267186bca490dc2d963b59e78cc88
Alexandr Gerasimov <phantom6391@yandex.ru>
2022-04-10 17:53:18 +0400

    Проба скрипта для сайдбара

--- scripts/webinar.js
+++ scripts/webinar.js
@@ -3,3 +3,21 @@ function goToTextButtonHandler() {
   document.location.href = "./text.html";
 }
 goToTextButton.addEventListener("click", goToTextButtonHandler);
+
+const menuItemList = document.querySelectorAll(".sidebar-menu__item-list");
+
+menuItemList.forEach(item => {
+  item.parentNode.addEventListener("click", function (evt) {
+    evt.stopPropagation();
+    item.classList.toggle("sidebar-menu__item-list_open");
+  });
+});
+
+const menuIcon = document.querySelectorAll(".icon__sidebar-menu");
+
+menuIcon.forEach(item => {
+  item.parentNode.addEventListener("click", function (evt) {
+    evt.stopPropagation();
+    item.classList.toggle("icon__sidebar-menu_rotated");
+  });
+});
