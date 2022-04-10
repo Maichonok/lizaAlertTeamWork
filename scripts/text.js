8d87d15d62f25d7e3155e992c72f1d19f4c31c0b
Alexandr Gerasimov <phantom6391@yandex.ru>
2022-04-10 17:54:31 +0400

    тест скрипта для второй страницы

--- scripts/text.js
+++ scripts/text.js
@@ -9,3 +9,21 @@ function goToWebinarButtonHandler() {
   document.location.href = "./webinar.html";
 }
 goToWebinarButton.addEventListener("click", goToWebinarButtonHandler);
+
+const menuItemList = document.querySelectorAll(".sidebar-menu__item-list");
+
+menuItemList.forEach(item => {
+  item.parentNode.addEventListener("click", function (evt) {
+    evt.stopPropagation();
+    item.classList.toggle("sidebar-menu__item-list_open");
+  });
+});
+
+const menuIcon = document.querySelectorAll(".icon__sidebar-menu");
+
+menuIcon.forEach(item => {
+  item.parentNode.addEventListener("click", function (evt) {
+    evt.stopPropagation();
+    item.classList.toggle("icon__sidebar-menu_rotated");
+  });
+});
